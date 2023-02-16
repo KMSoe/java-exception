1a612677b538fc6ff73ca4a812dccf5f2912d9f4
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2023-02-16 12:52:16 +0630

    throw exception

--- 02_Throw_Exception/src/MyReader.java
+++ 02_Throw_Exception/src/MyReader.java
@@ -1,13 +1,12 @@
 import java.io.FileNotFoundException;
 
-public class MyReader{
-	public void read() throws FileNotFoundException
-	{
-		int errorcode=1;
-		if(errorcode==1)
-		{
-			FileNotFoundException ex=new FileNotFoundException("File Not Found");
-			throw ex;
-		}
-	}
+public class MyReader {
+    public void read() throws FileNotFoundException {
+        int errorcode = 1;
+        
+        if (errorcode == 1) {
+            FileNotFoundException ex = new FileNotFoundException("File Not Found");
+            throw ex;
+        }
+    }
 }
